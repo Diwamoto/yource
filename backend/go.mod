5d3b0dace242d4a1d06263fc493cbc7bbf4212e3
Diwamoto <daiki.work.0118@gmail.com>
2021-01-13 11:42:23 +0900

    ライブリロードできるようにした

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module gin
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+)
--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,43 @@
 package main
 import (
+    "net/http"
+
     "github.com/gin-gonic/gin"
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+type User struct {
+    gorm.Model
+    Id int
+    Name string
+}
+
+type C struct {
+    Id int
+    Name string
+  }
+
 func main() {
+
+    db, err := gorm.Open("mysql", "user:password@tcp(db:3306)/yource?charset=utf8&parseTime=True&loc=Local")
+    if err != nil {
+        //panic("failed to connect database")
+    }
+    defer db.Close()
+
     r := gin.Default()
+    r.LoadHTMLGlob("template/*.tmpl")
     r.GET("/", func(c *gin.Context) {
-        c.String(200, "Hello Gin!!")
+        c.HTML(http.StatusOK, "index.tmpl", gin.H{
+            "a": "a",
+            "b": []string{"b_todo1","b_todo2"},
+            "c": []C{{1,"c_mika"},{2,"c_risa"}},
+            "d": C{3,"d_mayu"},
+            "e": true,
+            "f": false,
+            "h": false,
+          })
     })
-    r.Run(":8081")
+    r.Run(":3001")
 }

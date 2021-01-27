7d2943f67690a5b808cfa3af9bbeb672a1d30797
Diwamoto <daiki.work.0118@gmail.com>
2021-01-28 02:40:47 +0900

    トップページちょこっとつくった

---
+++ frontend/src/components/TopBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+  <v-app-bar
+        color="white"
+        dense
+      >
+        <v-app-bar-nav-icon>
+          <v-img
+            alt="Vuetify Logo"
+            class=""
+            contain
+            src="/img/logo.png"
+            transition="scale-transiton"
+            width="60"
+            height="40"
+          />
+        </v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Yource</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-menu
+          right
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+  
+          <v-list>
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              @click="() => {}"
+            >
+              <v-list-item-title>Option {{ n }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-app-bar>
+  </v-app>
+</template>
+
+<script></script>
+
+<style>
+.v-sheet .theme--light .v-toolbar .v-toolbar--dense .v-app-bar .v-app-bar--is-scrolled .white{
+  height:30px
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,46 @@
 <template>
-  <div class="home">
-    <v-container>
-      <v-img
-          alt="Vuetify Logo"
-          class=""
-          contain
-          src="/img/logo.png"
-          transition="scale-transiton"
-          width="100"
-        />
-    </v-container>
+  <div class="header">
+    <v-row>
+      <!-- logo -->
+      <!-- TODO:タイトルとロゴを統一した画像にする -->
+      
+      <v-col sm="1">
+        <router-link to="/summary">
+          <v-img
+            width="75"
+            class="main-logo_image"
+            src="/img/logo.png"
+          ></v-img>
+        </router-link>
+      </v-col>
+      <v-col sm="3">
+        <router-link to="/summary">
+        <v-img
+          width="130"
+          class="main-logo_name"
+          src="/img/logo_name.png"
+        ></v-img>
+         </router-link>
+      </v-col>
+     
+      <!-- /logo -->
+
+      <!-- menu -->
+      <v-col sm="1" offset-sm="3" class="menu-element">
+        <router-link to="/summary">概要</router-link>
+      </v-col>
+
+      <v-col sm="1" class="menu-element">
+        <router-link to="/howtojoin">参加方法</router-link>
+      </v-col>
+
+      <v-col sm="1" class="menu-element">
+        <router-link to="/join">会員登録</router-link>
+      </v-col>
+
+      <!-- /menu -->
+     
+    </v-row>   
   </div>
 </template>
 
@@ -21,3 +52,21 @@ export default {
   
 }
 </script>
+
+<style>
+.header{
+  margin-top: 30px;
+  margin-left: 70px; 
+}
+.main-logo_name{
+  margin-top: 16px;
+  margin-left: -20px;
+}
+.menu-element{
+  margin-top: 40px;
+}
+.menu-element a{
+  color: black;
+  text-decoration: none;
+}
+</style>

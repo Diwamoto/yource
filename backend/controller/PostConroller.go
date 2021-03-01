e85326567f205091d474bc2c98f839966fed028f
Diwamoto <daiki.work.0118@gmail.com>
2021-03-01 15:48:19 +0900

    [バックエンド]タイポミスを修正

--- backend/controller/PostConroller.go
+++ backend/controller/PostConroller.go
@@ -18,7 +18,7 @@ import (
 //POSTされた要素でデータを作成する
 func CreatePostAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 
 	//spaceidをintに変換
 	channelId, _ := strconv.Atoi(c.Param("id"))
@@ -35,11 +35,11 @@ func CreatePostAction(c *gin.Context) {
 	s.Created = time.Now()
 	s.Modified = time.Now()
 
-	msg, err := cm.Create(s)
+	msg, err := pm.Create(s)
 	//エラーじゃなければuserの情報を返す
 	if !err {
 		postId, _ := strconv.Atoi(msg[0])
-		post, _ := cm.GetById(postId)
+		post, _ := pm.GetById(postId)
 
 		//ユーザのメールアドレス死活監視トークンを生成する。
 
@@ -55,12 +55,12 @@ func CreatePostAction(c *gin.Context) {
 //GETで指定IDの投稿を返す
 func GetPostAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 
 	id, _ := strconv.Atoi(c.Param("id"))
-	ch, err := cm.GetById(id)
+	post, err := pm.GetById(id)
 	if !err {
-		c.JSON(http.StatusOK, ch)
+		c.JSON(http.StatusOK, post)
 	} else {
 		c.JSON(http.StatusNotFound, []string{})
 	}
@@ -70,10 +70,10 @@ func GetPostAction(c *gin.Context) {
 //GETで指定投稿の投稿を全て返す
 func GetPostByChannelIdAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 
 	channelId, _ := strconv.Atoi(c.Param("id"))
-	posts, err := cm.GetByChannelId(channelId)
+	posts, err := pm.GetByChannelId(channelId)
 	if !err {
 		c.JSON(http.StatusOK, posts)
 	} else {
@@ -85,10 +85,10 @@ func GetPostByChannelIdAction(c *gin.Context) {
 //GETで指定ユーザの投稿を全て返す
 func GetPostByUserIdAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 
-	channelId, _ := strconv.Atoi(c.Param("id"))
-	posts, err := cm.GetByChannelId(channelId)
+	userId, _ := strconv.Atoi(c.Param("id"))
+	posts, err := pm.GetByUserId(userId)
 	if !err {
 		c.JSON(http.StatusOK, posts)
 	} else {
@@ -100,11 +100,11 @@ func GetPostByUserIdAction(c *gin.Context) {
 //GETで全ての投稿の情報を取得する
 func GetAllPostAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 
-	users, err := cm.GetAll()
+	posts, err := pm.GetAll()
 	if !err {
-		c.JSON(http.StatusOK, users)
+		c.JSON(http.StatusOK, posts)
 	} else {
 		c.JSON(http.StatusNotFound, []string{})
 	}
@@ -114,18 +114,18 @@ func GetAllPostAction(c *gin.Context) {
 //PUTでフォームの情報から投稿の情報を更新する
 func UpdatePostAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 
 	id, _ := strconv.Atoi(c.Param("id"))
 	//を取得し、取得できたら更新をかける
-	_, err := cm.GetById(id)
+	_, err := pm.GetById(id)
 	if !err {
 		//フォームから更新内容を取得し投稿構造体を作成
-		var ch model.Post
-		ch.Content = c.PostForm("Content")
-		msg, err2 := cm.Update(id, ch)
+		var post model.Post
+		post.Content = c.PostForm("Content")
+		msg, err2 := pm.Update(id, post)
 		if !err2 {
-			r, _ := cm.GetById(id)
+			r, _ := pm.GetById(id)
 			c.JSON(http.StatusOK, r)
 		} else {
 			c.JSON(http.StatusConflict, msg)
@@ -138,9 +138,9 @@ func UpdatePostAction(c *gin.Context) {
 //投稿の削除アクション
 func DeletePostAction(c *gin.Context) {
 
-	cm := model.NewPostModel("default")
+	pm := model.NewPostModel("default")
 	postId, _ := strconv.Atoi(c.Param("id"))
-	msg, err := cm.Delete(postId)
+	msg, err := pm.Delete(postId)
 	if !err {
 		c.JSON(http.StatusOK, msg)
 	} else {

61aa24cb303c3d2810ec3de16becc9e6ce7e12f3
Diwamoto <daiki.work.0118@gmail.com>
2021-03-06 18:06:01 +0900

    デプロイスクリプトを調整

--- deployment.sh
+++ deployment.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# setup env
+source ~/.profile
+
 # checkout 
 cd ~/yource
 git pull
 
+# setup go modules
+cd backend
+go mod tidy
+
 # deploy frontend
-cd frontend
+cd ../frontend
+npm install 
 npm run build

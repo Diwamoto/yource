a0595d6ea53ca0902b4a5f0e8dbbb88f9c2f7467
Diwamoto <daiki.work.0118@gmail.com>
2021-03-30 23:09:23 +0900

    メンテナンスページとメンテナンスモードを追加

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,58 +4,87 @@ import NotFoundComponent from '@/components/NotFoundComponent.vue'
 
 Vue.use(VueRouter)
 
+var routes = []
 
-const routes = [
-  {
+//メンテナンス中の場合はメンテナンスページに飛ばし、それ以外には行かせない
+if (process.env.VUE_APP_IS_MAINTENANCE == "true") {
+  routes = [{
     path: '/',
-    name: 'Top',
-    component: () => import('../views/Top.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
-  },
-  {
-    path: '/privacy',
-    component: () => import('../views/PrivacyPolicy.vue'),
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/home', component: () => import('../views/Home.vue'),
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/new',
-    component: () => import('../views/Create.vue'),
-    meta: { requiresAuth: true },
-  },
-  {
-    path: '/register',
-    component: () => import('../views/Register.vue'),
-  },
-  {
-    path: '/verify',
-    component: () => import('../views/Verify.vue'),
-  },
-  { path: '/user', component: () => import('../views/User.vue'),
-    children: [
-      {
-        // /user/:id/profile がマッチした時に
-        // UserProfile は User の <router-view> 内部で描画されます
-        path: 'profile',
-        component: () => import('@/components/user/Profile.vue'),
+    name: 'maintenance',
+    component: () => import('../views/Maintenance.vue')
+  }, ]
+
+} else {
+
+  routes = [{
+      path: '/',
+      name: 'Top',
+      component: () => import('../views/Top.vue')
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: () => import('../views/About.vue')
+    },
+    {
+      path: '/privacy',
+      component: () => import('../views/PrivacyPolicy.vue'),
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: () => import('../views/Login.vue')
+    },
+    {
+      path: '/home',
+      component: () => import('../views/Home.vue'),
+      meta: {
+        requiresAuth: true
       },
-      { path: '', component: () => import('@/components/user/Main.vue')},
-    ]
-  },
-  //ブラウザバック対策
-  { path: '*', name:"404", component: NotFoundComponent }
-]
+    },
+    {
+      path: '/new',
+      component: () => import('../views/Create.vue'),
+      meta: {
+        requiresAuth: true
+      },
+    },
+    {
+      path: '/register',
+      component: () => import('../views/Register.vue'),
+    },
+    {
+      path: '/verify',
+      component: () => import('../views/Verify.vue'),
+    },
+    {
+      path: '/maintenance',
+      component: () => import('../views/Maintenance.vue'),
+    },
+    {
+      path: '/user',
+      component: () => import('../views/User.vue'),
+      children: [{
+          // /user/:id/profile がマッチした時に
+          // UserProfile は User の <router-view> 内部で描画されます
+          path: 'profile',
+          component: () => import('@/components/user/Profile.vue'),
+        },
+        {
+          path: '',
+          component: () => import('@/components/user/Main.vue')
+        },
+      ]
+    },
+    //ブラウザバック対策
+    {
+      path: '*',
+      name: "404",
+      component: NotFoundComponent
+    }
+  ]
+
+}
 
 const router = new VueRouter({
   mode: 'history',
@@ -74,10 +103,14 @@ router.beforeEach((to, from, next) => {
       // TODO:エラーメッセージのデザインを作る
       //
       /////////////////////////////////
-      Vue.$cookies.set("msg", "続けるにはログインが必要です。", {expires: "1H",})
+      Vue.$cookies.set("msg", "続けるにはログインが必要です。", {
+        expires: "1H",
+      })
       next({
         path: '/login',
-        query: { redirect: to.fullPath }
+        query: {
+          redirect: to.fullPath
+        }
       })
     } else {
       next()
@@ -87,7 +120,7 @@ router.beforeEach((to, from, next) => {
   }
 })
 
-function isLogin(){
+function isLogin() {
   return Vue.$cookies.get("token") != null
 }
 
---
+++ frontend/src/views/Maintenance.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="maintenance">
+    <div class="maintenance-box rounded-lg">
+      <h1 class="maintenance-title">メンテナンス中</h1>
+      <div class="maintenance-body-wrapper">
+        <h5 class="maintenance-main-title">Yourceは現在メンテナンス中です。ご迷惑をおかけして申し訳ありません。</h5>
+        <div class="maintenance-body maintenance-img-wrapper">
+          <v-img class="maintenance-img" src="/img/maintenance.svg"></v-img>
+        </div>
+        <div style="margin-top:20px;text-align:center;">お問い合わせ先: <a href="https://diwamoto.dev#contact" target="_brank">diwamoto.dev</a></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "maintenance",
+};
+</script>
+
+<style>
+.maintenance {
+  height: 100%;
+  background-color: #f3f3f3;
+}
+.maintenance-box {
+  background-color: white;
+  margin: 15vh auto 0 auto;
+  padding-bottom: 10vh;
+  width: 50vw;
+  opacity: 0;
+  box-shadow: 0 6px 6px -3px rgba(0, 0, 0, 0.2),
+    0 10px 14px 1px rgba(0, 0, 0, 0.14), 0 4px 18px 3px rgba(0, 0, 0, 0.12) !important;
+  animation-name: maintenance;
+  animation-duration: 0.5s;
+  animation-timing-function: ease-out;
+  animation-fill-mode: forwards;
+}
+.maintenance-title {
+  padding: 2% 0 2% 0;
+  text-align: center;
+  border-width: 0 0 2px 0;
+  border-style: solid;
+  border-color: #f0f0f0;
+}
+.maintenance-main-title {
+  font-size: 1.2em;
+  padding: 2% 0 2% 0;
+  text-align: center;
+}
+.maintenance-img-wrapper {
+  margin: 0 2vw 0 2vw;
+}
+.maintenance-img {
+  margin: 0 5vw 0 5vw;
+  width: 70%;
+}
+.maintenance-subtitle {
+  padding: 3% 10% 0 10%;
+}
+.maintenance-body-wrapper {
+  padding: 6% 10% 0 10%;
+}
+.maintenance-body {
+  padding: 0 5% 0 5%;
+}
+@keyframes maintenance {
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
